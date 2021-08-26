25f5a9ba151f3ad66175f5548a07062e96661536
ammar <azml1@gmail.com>
2021-08-26 21:27:54 +0500

    a

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import axios from "axios";
+import SingleProduct from "./SingleProduct"
 const Products = () => {
-    const [products, setProducts]=React.useState([])
+    const [products, setProducts]=React.useState([]);
+    const getData = () =>{
+        axios.get("http:/localhost:4000/api/products").then((res)=>{
+            
+        })
+    }
     return ( <div>
         <h1>Products</h1>
-        <p>asdasd</p>
-
+        {/* <SingleProduct product={products[0]}/>
+        <SingleProduct product={products[1]}/> */}
+        {products.length == 0?(<p>there are no Products</p>):(<div>{products.map((product,index ) =>    <SingleProduct key={index} product={product}/>
+            
+        )}</div> )}
+        
     </div> );
 }
  
